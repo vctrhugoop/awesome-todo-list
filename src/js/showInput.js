f971c08303fed9b685678b09d162e8ea56ec1d0a
vctrhugoop <vhdestroy@hotmail.com>
2023-12-16 09:59:48 -0300

    ✨ feat: implement cancel btn

--- src/js/showInput.js
+++ src/js/showInput.js
@@ -1,8 +1,13 @@
 const addNewTaskButton = document.getElementById('btn-new-task');
+const cancelButton = document.getElementById('cancel-btn');
 const newTaskInput = document.querySelector('.input-wrapper');
 
 addNewTaskButton.addEventListener('click', () => {
   newTaskInput.classList.remove('hide');
+});
+
+cancelButton.addEventListener('click', () => {
+  newTaskInput.classList.add('hide');
 });
 
 window.addEventListener('keydown', e => {
