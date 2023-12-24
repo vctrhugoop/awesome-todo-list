165eb3a274eb625a802504d9bd2dfaae2204691c
vctrhugoop <vhdestroy@hotmail.com>
2023-12-24 09:25:12 -0300

    ✨ feat: refanzedo as cagadas

--- src/js/NewTask.js
+++ src/js/NewTask.js
@@ -2,24 +2,20 @@ export class NewTask {
   constructor(root) {
     this.root = document.querySelector(root);
     this.load();
-    this.create();
   }
 
   load() {
-    this.entries = JSON.parse(localStorage.getItem('@tasks:')) || [
-      {
-        title: 'teste',
-        description: 'test',
-      },
-      {
-        title: 'teste',
-        description: 'test',
-      },
-    ];
+    this.entries = JSON.parse(localStorage.getItem('@tasks:')) || [];
+  }
+
+  save() {
+    localStorage.setItem('@tasks:', JSON.stringify(this.entries));
   }
 
   add(entrie) {
     this.entries = [entrie, ...this.entries];
+    this.update();
+    this.save();
   }
 
   delete(data) {
@@ -28,6 +24,7 @@ export class NewTask {
     );
     this.entries = filteredEntries;
     this.update();
+    this.save();
   }
 }
 
@@ -38,6 +35,7 @@ export class NewTaskView extends NewTask {
     this.taskList = this.root.querySelector('main .task-list ul');
 
     this.update();
+    this.getData();
   }
 
   update() {
@@ -61,6 +59,10 @@ export class NewTaskView extends NewTask {
 
       this.taskList.append(task);
     });
+
+    this.countTasks();
+    this.priority();
+    this.search();
   }
 
   createTask() {
@@ -121,7 +123,7 @@ export class NewTaskView extends NewTask {
     return createlist;
   }
 
-  create() {
+  getData() {
     const form = document.querySelector('form');
     const inputTaskTitle = document.querySelector('#task-title');
     const inputTaskDescription = document.querySelector('#task-description');
@@ -148,6 +150,120 @@ export class NewTaskView extends NewTask {
   removeAllTaskList() {
     this.taskList.querySelectorAll('ul').forEach(list => {
       list.remove();
+    });
+  }
+
+  countTasks() {
+    const createdTask = document.querySelectorAll('.task-wrapper');
+    const buttonCheck = document.querySelectorAll('.btn-check');
+    const changeIconFill = document.querySelectorAll('.btn-check .ph');
+    const changeIconCheck = document.querySelectorAll('.btn-check .ph-circle');
+    const countCreated = document.querySelector('.created-task span');
+    const countConcluded = document.querySelector('.concluded-task span');
+
+    countCreated.textContent = createdTask.length;
+
+    countConcluded.textContent = `0
+    de ${createdTask.length}`;
+
+    for (let i = 0; i < createdTask.length; i++) {
+      buttonCheck[i].addEventListener('click', () => {
+        createdTask[i].classList.toggle('concluded');
+        changeIconFill[i].classList.toggle('ph-fill');
+        changeIconCheck[i].classList.toggle('ph-circle');
+        changeIconCheck[i].classList.toggle('ph-check-circle');
+
+        countConcluded.textContent = `${
+          document.querySelectorAll('.concluded').length
+        } de ${createdTask.length}`;
+      });
+    }
+  }
+
+  priority() {
+    const buttonPriority = document.querySelectorAll('.tag');
+    const tagPriority = document.querySelectorAll('.tag span');
+    const priorityBox = document.querySelectorAll('.priority');
+
+    for (let i = 0; i < priorityBox.length; i++) {
+      buttonPriority[i].addEventListener('click', () => {
+        priorityBox[i].classList.toggle('hide');
+        priorityBox[i].addEventListener('click', e => {
+          if (e.target.tagName === 'P') {
+            const tag = e.target.innerText;
+            tagPriority[i].textContent = tag;
+
+            switch (tagPriority[i].textContent) {
+              case 'BAIXA':
+                buttonPriority[i].classList.add('low');
+                buttonPriority[i].classList.remove('medium');
+                buttonPriority[i].classList.remove('high');
+                priorityBox[i].classList.add('hide');
+                break;
+
+              case 'MÉDIA':
+                buttonPriority[i].classList.add('medium');
+                buttonPriority[i].classList.remove('low');
+                buttonPriority[i].classList.remove('high');
+                priorityBox[i].classList.add('hide');
+                break;
+
+              case 'ALTA':
+                buttonPriority[i].classList.add('high');
+                buttonPriority[i].classList.remove('medium');
+                buttonPriority[i].classList.remove('low');
+                priorityBox[i].classList.add('hide');
+                break;
+              default:
+                buttonPriority[i].classList.add('default');
+                buttonPriority[i].classList.remove('high');
+                buttonPriority[i].classList.remove('medium');
+                buttonPriority[i].classList.remove('low');
+                priorityBox[i].classList.add('hide');
+                tagPriority[i].textContent = 'Adicione uma prioridade';
+                break;
+            }
+          }
+        });
+        window.addEventListener('keydown', e => {
+          if (e.key === 'Escape') {
+            e.preventDefault();
+            priorityBox[i].classList.add('hide');
+          }
+        });
+      });
+    }
+  }
+
+  search() {
+    const filterElement = document.querySelector('#search-task');
+
+    const tasks = document.querySelectorAll('.task-wrapper');
+
+    filterElement.addEventListener('input', () => {
+      if (filterElement.value != '') {
+        for (let task of tasks) {
+          let title = task.querySelector('h3');
+          let description = task.querySelector('p');
+          title = title.textContent.toLowerCase();
+          description = description.textContent.toLowerCase();
+
+          let filterText = filterElement.value;
+
+          if (
+            !title.includes(filterText) &&
+            !description.includes(filterText)
+          ) {
+            task.style.display = 'none';
+          } else {
+            task.style.display = 'block';
+          }
+        }
+      } else {
+        for (let task of tasks) {
+          task.style.display = 'block';
+        }
+      }
     });
   }
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,4 @@
 import { NewTaskView } from './NewTask.js';
-import { concluded } from './concluded.js';
-import { priority } from './priority.js';
-import { search } from './Search.js';
 import './showInput.js';
-import './createTask.js';
 
 new NewTaskView('#app');
-concluded();
-priority();
-search();
